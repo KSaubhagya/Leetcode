e00b7a1264a7a4247838b4ebfc610b9a63b25856
Kavindi Saubhagya <110480920+KSaubhagya@users.noreply.github.com>
2026-04-21 20:08:14 +0530

    Update duplicate

--- TwoPointerDuplicate.py
+++ TwoPointerDuplicate.py
@@ -5,7 +5,7 @@ class Solution(object):
     
         i = 0  # pointer for unique elements
         
-        for j in range(1, len(nums)):
+        for j in range(1, len(nums)): 
             if nums[j] != nums[i]:
                 i += 1
                 nums[i] = nums[j]
